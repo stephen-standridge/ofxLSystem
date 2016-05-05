1825a69c5d1c4add0a5dbc9bb2119b3dbbf95a6c
edapx <lastexxit@gmail.com>
2016-05-05 12:43:09 +0200

    cap sizes

--- src/ofxLSBranch.h
+++ src/ofxLSBranch.h
@@ -8,6 +8,7 @@ public:
         begin = _begin; // it is where the cylinder starts
         end = _end;     // it is where the cylinder ends
         capSizes = _capSizes; // the length of the ray of the 2 caps of the cylinder
+        // the first one defines the bottom cap, the second one defines top cap
     };
     ofNode begin;
     ofNode end;
